17734b7f7cfaefcee942d92e0395b4c9bca09a81
Iheanyi Oziegbe <neoconstel@gmail.com>
2022-10-14 11:46:08 +0100

    'Add Books' and 'View Orders' for admin-portal

--- library/urls.py
+++ library/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('librarian/', views.admin_portal, name='admin-portal'),
+    path('librarian/', views.AdminPortal.as_view(), name='admin-portal'),
+    path('librarian/add-books/', views.AddBooks.as_view(), name='add-books'),
+    path('librarian/view-orders/', views.ViewOrders.as_view(), name='view-orders'),
+
+
     path('student/', views.student_portal, name='student-portal')
 ]
--- library/views.py
+++ library/views.py
@@ -1,17 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+
+from .models import Book, Order
+from .forms import BookForm
 
 
 def home(request):
-    return render(request, "library/index.html")
+    return render(request, 'library/index.html')
 
 
-def admin_portal(request):
-    return render(request, 'library/admin_portal.html')
+# def admin_portal(request):
+#     books = Book.objects.all() 
+#     context = {
+#          'books': books,
+#          'total_book_titles': books.count(),
+#          'total_available_books': sum([book.quantity for book in books])
+#     }
+#     return render(request, 'library/admin_portal.html', context=context)
 
 
 def student_portal(request):
     return render(request, 'library/student_portal.html')
+
+
+class AddBooks(CreateView):
+    model = Book
+    fields = '__all__'
+    success_url = reverse_lazy('admin-portal')
+
+
+class AdminPortal(ListView):
+    model = Book
+    template_name = 'library/admin_portal.html'
+    context_object_name = 'books'
+
+
+class ViewOrders(ListView):
+    model = Order
+    context_object_name = 'orders'
 
 
 
